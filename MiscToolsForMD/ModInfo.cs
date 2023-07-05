d147f571ac4224909d32724d8180b56bc1f1e8d3
Zhang Hua <zhanghua.00@qq.com>
2023-07-05 15:31:26 +0800

    MiscToolsForMD depends MiscToolsForMD.Events now

--- MiscToolsForMD/ModInfo.cs
+++ MiscToolsForMD/ModInfo.cs
@@ -3,6 +3,7 @@ using MelonLoader;
 [assembly: MelonInfo(typeof(MiscToolsForMD.MiscToolsForMD), "MiscToolsForMD", "0.1.0", "MiscToolsForMD")]
 [assembly: MelonGame("PeroPeroGames", "MuseDash")]
 [assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.IL2CPP)]
+[assembly: MelonAdditionalDependencies("MiscToolsForMD.Events")]
 // Using MelonLoader v0.6.1 at least
 [assembly: VerifyLoaderVersion("0.6.1", true)]
 // Supports game v3.4.0
